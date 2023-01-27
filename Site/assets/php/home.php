6d2f32eb9fed7f301f629c58b208cd6a54f45b60
Kyle Anderson <kyleandersontx@gmail.com>
2023-01-27 16:16:15 -0600

    Redesign in HTML in CSS.

--- Site/assets/php/home.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo '<div class="container">';
-	echo '<div class="spacer"></div>';
-	echo '<div class="title"></div>';
-	echo '<div class="message"><h2>Under Construction</h2></div>';
-	echo '<div class="photoCred">Photo by <a href="https://unsplash.com/@vorosbenisop">Benjamin Voros</a> on <a href="https://unsplash.com/">Unsplash</a></div>';
-echo '</div>';
-?>
